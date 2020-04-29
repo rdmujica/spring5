466367f9c182a4c6021f2f0a528927f21108604c
rafamujica <rdmujica86@gmail.com>
2020-04-28 21:35:21 -0400

    Seccion6: Interceptorees

---
+++ spring-boot-horario/src/main/java/com/bolsaideas/springboot/horariointerceptor/app/SpringBootHorarioApplication.java
@@ -0,0 +1,13 @@
+package com.bolsaideas.springboot.horariointerceptor.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHorarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHorarioApplication.class, args);
+	}
+
+}

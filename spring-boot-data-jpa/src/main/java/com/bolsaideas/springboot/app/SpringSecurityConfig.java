524fbfa1835eeb46c2264bd76ee5608b59d92fb9
rafamujica <rdmujica86@gmail.com>
2020-05-06 02:05:43 -0400

    Sección 15: Spring Security: Autenticación con JPA

--- spring-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/SpringSecurityConfig.java
+++ spring-boot-data-jpa/src/main/java/com/bolsaideas/springboot/app/SpringSecurityConfig.java
@@ -5,6 +5,7 @@ package com.bolsaideas.springboot.app;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -13,6 +14,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.User.UserBuilder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -30,6 +32,10 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	
 	@Autowired
 	DataSource dataSource;
+	
+	@Autowired
+	@Qualifier("jpaUserDetailService")
+	UserDetailsService userDetailsService;
 
 	/* Usuarios en memoria
 	 * @Autowired public void configurerGlobal(AuthenticationManagerBuilder builder)
@@ -42,15 +48,21 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	 * .withUser(users.username("rafael").password("123456").roles("USER")); }
 	 */
 	
+	/* JDBC
+	 * @Autowired public void configurerGlobal(AuthenticationManagerBuilder builder)
+	 * throws Exception { builder .jdbcAuthentication() .dataSource(dataSource)
+	 * .passwordEncoder(passwordEncoder)
+	 * .usersByUsernameQuery("select username,password,enabled from users where username=?"
+	 * )
+	 * .authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?"
+	 * ); }
+	 */
 	@Autowired
 	public void configurerGlobal(AuthenticationManagerBuilder builder) throws Exception {
 		builder
-		.jdbcAuthentication()
-		.dataSource(dataSource)
-		.passwordEncoder(passwordEncoder)
-		.usersByUsernameQuery("select username,password,enabled from users where username=?")
-		.authoritiesByUsernameQuery("select u.username, a.authority from authorities a inner join users u on (a.user_id=u.id) where u.username=?");
+		.userDetailsService(userDetailsService);
 	}
+
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
